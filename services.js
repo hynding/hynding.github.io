c8eb3bd31c27f1c28376132251f7442b6a60804c
Steve Hynding <hynding@users.noreply.github.com>
2021-01-20 02:31:55 -0800

    update services.js

--- services.js
+++ services.js
@@ -1,6 +1,14 @@
+export const isArray = Array.isArray
+export const isObject = value => typeof value === 'object'
+
+
 export const createApp = Vue.createApp
 
 export const requestGet = axios.get
 export const requestPost = axios.post
 export const requestPut = axios.put
 export const requestDelete = axios.delete
+
+export const render = (...args) = {
+  if (args.length && isArray(args[0]))
+}
