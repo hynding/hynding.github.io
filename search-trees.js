3b0b414782fa61b9538892abc4e93deb07cd480c
Steve Hynding <hynding@users.noreply.github.com>
2021-02-18 00:50:02 -0800

    update search-trees.js

--- search-trees.js
+++ search-trees.js
@@ -1,19 +1,25 @@
-const byObjectKey = (key, desc = true => (a,b)=>(b[key]-a[key])*(desc?1:-1)
+const byObjectKey = (key, desc = true) => (a,b)=>(b[key]-a[key])*(desc?1:-1)
+
+const byObjectKeys = keys => (a,b) => keys.reduce((result, key)=>result||(b[key]-a[key]), 0)
+
+
+
 const sort = list => ({
-  by: options => {
-    if (typeof options === 'string') {
-      return list.slice(0).sort(byObjectKey(options))
-    }
-    
-    
+  by: options => list.slice(0).sort(typeof options === 'string'
+    ? byObjectKey(options)
+    : byObjectKeys(options)
   }
 })
+
+const toTree = (tree, ) => []
 
 const createSearchTree = options => {
   const {
     rows,
     cols,
+    indices, // sort order array of col indices
   } = options
   
+  sort(rows).by(indices).reduce(toTree, [])
   
 }
